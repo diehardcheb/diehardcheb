ebf669384b50f7efc8c198900eb6f45f34eaa657
Vladimir Nestarov <nesterovva@vtb.ru>
2025-02-04 17:19:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+# Токен, выданный BotFather в телеграмме
+API_TOKEN = 'BOT TOKEN HERE'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+executor.start_polling(dp, skip_updates=True)
